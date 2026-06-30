b3512ced605078aa822ec87a925ebbeeff27dcf0
devturakhiya2006 <dev.turakhiya2006@gmail.com>
2026-06-30 23:54:20 +0530

    main

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("JavaScript is successfully linked!");
+
+// Add interactivity here
+document.querySelector('h1').addEventListener('click', () => {
+    alert("Project collaboration in progress!");
+});
